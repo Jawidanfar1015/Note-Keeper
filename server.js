9a7383255f0e0f57c14d9d969652af713a46e1cf
Jawid Noori <Hajikaka4030@gmail.com>
2022-06-17 22:52:44 -0700

    port assigned and middleware selected

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const util = require('util');
 const { json } = require('express');
 const { notStrictEqual } = require('assert');
 
+// setting up server
+const app = express();
+// assigning port for the server
+const PORT = process.env.PORT || 3334;
+
+// Static Middleware, using json and urlencoded true
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
