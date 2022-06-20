1ff96a1d8dedf77a30b4d2eb7881e9b59fecfa74
Jawid Noori <Hajikaka4030@gmail.com>
2022-06-19 21:44:59 -0700

    extra code removed

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const { json } = require('express');
-const { notStrictEqual } = require('assert');
+
 
 // setting promises and variables for reading and writing file
 const readFileAsync = util.promisify(fs.readFile);
